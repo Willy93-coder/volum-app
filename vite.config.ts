5c97c14f415d3f620f2ec7a3d34b32f87064fcab
Guillermo Cifre <guillecg13@gmail.com>
2026-05-03 11:18:29 +0200

    feat: update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/volum-app/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
