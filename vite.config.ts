1bddea87c6994c08ab6642f6e4ed84dd249cdb88
Guillermo Cifre <guillecg13@gmail.com>
2026-05-07 21:20:38 +0200

    feat: add Rust/WASM support with vite-plugin-wasm and update .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/volum-app/',
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), wasm()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
